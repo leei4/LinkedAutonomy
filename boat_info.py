33f6c0d5722624d116b0ef1b483311248ad0efff
leei4 <35853303+leei4@users.noreply.github.com>
2018-06-18 15:20:17 +0000

    Add files via upload
    Upload of code to control boat logic.
    boat_info contains the information for the boat  to make calculations (ie. heading and wind)

---
+++ boat_info.py
@@ -0,0 +1,21 @@
+#where the boat is facing
+boatHeading = 0
+
+#wind info
+relativeWindheading = 0
+windHeading = 0
+upwind = false
+
+#boat latitude and longitude
+startLat = 41.13012
+startLong = -73.263
+
+#destination latitude and longitude
+finalLat = 41.131412
+finalLong = -73.28398 
+
+#distance to destination
+distancetoDest = 100
+
+#rudder controls
+RudderAngle = 90
